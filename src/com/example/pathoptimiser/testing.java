d53f026e2822b90d4346f6ab93c4651b2af89daa
Pawandeep23 <er.pawandeep23@gmail.com>
2015-03-09 14:59:05 -0400

    learning to push and pull by pawandeep

--- src/com/example/pathoptimiser/testing.java
+++ src/com/example/pathoptimiser/testing.java
@@ -3,6 +3,6 @@ learning to push and pull
 learning to push and pull - Rupam
 learning to push and pull - Gurteshwar Sandhu
 =======
-learning to push and pull 
+learning to push and pull - Pawandeep Singh
 Learning to push and pull Elena
->>>>>>> f88098b1ff92ab044b507188b3597c62190ed84a
+
